b691dd598aece99e74fc9da146a2bcf09f62e9a3
Santeri Siirilä <santeri@debian.home>
2019-05-15 21:53:21 +0300

    built npm and structured project

---
+++ build/precache-manifest.f7cfd8905c31b58f377e8f5b499f21ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9954747a6bc4b85ac9317a2ca8703c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b893aa2f63bd4a2f015b",
+    "url": "/static/css/2.a414b599.chunk.css"
+  },
+  {
+    "revision": "b893aa2f63bd4a2f015b",
+    "url": "/static/js/2.cee2adc2.chunk.js"
+  },
+  {
+    "revision": "ec50feef5ca15f218cbe",
+    "url": "/static/js/main.4ae1d39b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/containers/ArticleListView.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import axios from 'axios';
+import Article from '../components/Article';
+import CustomForm from '../components/Form';
+import { connect } from 'react-redux';
+
+class ArticleList extends React.Component {
+
+    state = {
+        article: [],
+    }
+
+    componentWillReceiveProps(newProps) {
+        if (newProps.token) {
+            axios.defaults.headers = {
+                "Content-Type": "application/json",
+                Authorization: newProps.token
+            }
+            axios.get('http://127.0.0.1:8000/api/')
+                .then(res => {
+                    this.setState({
+                        article: res.data
+                    });
+                })
+                .catch(function (error) {
+                    if (error.response) {
+                        console.log(error.response.data);
+                        console.log(error.response.status);
+                        console.log(error.response.headers);
+                    }
+                })
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <Article data={this.state.article}/>
+                    <br/>
+                    {this.props.isAuthenticated ? <h2> Create an article</h2> : <h2></h2>}
+                    <CustomForm
+                    requestType="post"
+                    articleID={null}
+                    btnText="Create" />
+                </div>
+            </div>
+        )
+    }
+}
+const mapStateToProps = state => {
+    return {
+        token: state.token
+    }
+}
+
+export default connect(mapStateToProps)(ArticleList);
